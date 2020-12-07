f5f5365d566d5b8cc14647cfabffd6d36d562121
Joshua French <just-joshing@github.com>
2020-12-07 10:23:17 -0700

    new stuff

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const core = require('@actions/core');
 const https = require('https');
+const fs = require('fs');
 
 async function run() {
   try {
-    const dd_api_key = core.getInput('datadog-api-key', { required: true });
-    const dd_app_key = core.getInput('datadog-app-key', { required: true });
+    const datadogApiKey = core.getInput('datadog-api-key', { required: true });
+    const datadogAppKey = core.getInput('datadog-app-key', { required: true });
 
-    console.log(`API ${dd_api_key.length}`);
-    console.log(`APP ${dd_app_key.length}`);
-
-    return new Promise(resolve => {
+    const dashboards = await new Promise((resolve, reject) => {
       https.get('https://api.datadoghq.com/api/v1/dashboard', {
         headers: {
           'Content-Type': 'application/json',
-          'DD-API-KEY': dd_api_key,
-          'DD-APPLICATION-KEY': dd_app_key,
+          'DD-API-KEY': datadogApiKey,
+          'DD-APPLICATION-KEY': datadogAppKey,
         }
       }, resp => {
         let data = "";
@@ -24,31 +22,29 @@ async function run() {
 
         resp.on('end', () => {
           data = JSON.parse(data);
-          console.log(data);
-
-          if (data.dashboards) {
-            console.log(`Length ${data.dashboards.length}`);
-
-            if (data.dashboards.length > 0) {
-              console.log(`Length ${data.dashboards[0].author_handle}`);
-            }
-          } else {
-            core.setFailed("No dashboards");
+          if (!data.dashboards) {
+            reject("Dashboards not returned. Likely a request error to Datadog. Check your inputs to this action.");
           }
 
-          const time = (new Date()).toTimeString();
-          core.setOutput("time", time);
-
-          resolve();
+          resolve(data.dashboards);
         });
-      });
+      })
+      .on('error', error => reject(error));
     });
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    console.log("Filtering dashboards");
+    const filteredDashboards = dashboards.filter(d => d.title.toLowerCase().includes("secret") && d.title.toLowerCase().includes("scanning"));
+    console.log(`${filteredDashboards.length} dashboards found`);
+
+    console.log("Writing dashboards");
+    fs.writeFile("dashboards.txt",
+      filteredDashboards.map(({ title, url }) => ({ title, url })),
+      err => { if (err) throw err; }
+    );
+
+    console.log("Done writing");
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message || error);
   }
 }
 
