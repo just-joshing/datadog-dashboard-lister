caaf2058d1380cfaef7725d8eb8496a5d98218ab
Joshua French <just-joshing@github.com>
2021-03-17 09:56:56 -0600

    Revert "Double escape newlines"
    This reverts commit 5993a81b0584affa66aac2b3cc7b39986e6393b5.

--- index.js
+++ index.js
@@ -62,8 +62,7 @@ async function run() {
     console.log(`${filteredDashboards.length} dashboards found`);
 
     console.log("Writing dashboards to output variable");
-    output = JSON.stringify(filteredDashboards.map(({ title, description, url }) => ({ title, description, url: `https://app.datadoghq.com${url}` })));
-    output = output.split("\n").join("\\n")
+    output = JSON.stringify(filteredDashboards.map(({ title, description, url }) => ({ title, description, url: `https://app.datadoghq.com${url}` }))),
     core.setOutput("dashboards", output);
     console.log("Done writing");
   } catch (error) {
